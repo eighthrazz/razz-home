08f6f707c98f3324b644a78e92f57171ff9eaa04
Robert Rasgorshek <robert.rasgorshek@ngc.com>
2019-03-03 11:23:35 -0600

    create ping micro service

---
+++ ping-service/src/main/java/com/razz/ping/PingTestEvent.java
@@ -0,0 +1,88 @@
+package com.razz.ping;
+
+import java.util.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "PingTestEvent")
+public class PingTestEvent {
+
+  @Id private String id;
+  private String host;
+  private Double millis;
+  private Long date;
+
+  public PingTestEvent(String host, Double millis, Long date) {
+    this.host = host;
+    this.millis = millis;
+    this.date = date;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public Double getMillis() {
+    return millis;
+  }
+
+  public void setMillis(Double millis) {
+    this.millis = millis;
+  }
+
+  public Long getDate() {
+    return date;
+  }
+
+  public void setDate(Long date) {
+    this.date = date;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PingTestEvent that = (PingTestEvent) o;
+    return Objects.equals(id, that.id)
+        && Objects.equals(host, that.host)
+        && Objects.equals(millis, that.millis)
+        && Objects.equals(date, that.date);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, host, millis, date);
+  }
+
+  @Override
+  public String toString() {
+    return "PingTestEvent{"
+        + "id='"
+        + id
+        + '\''
+        + ", host='"
+        + host
+        + '\''
+        + ", millis="
+        + millis
+        + ", date="
+        + date
+        + '}';
+  }
+}
---
+++ ping-service/src/main/java/com/razz/ping/PingTestHandler.java
@@ -0,0 +1,65 @@
+package com.razz.ping;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PingTestHandler {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(PingTestHandler.class);
+
+  private final Map<String, InetAddress> inetAddressMap;
+
+  @Value("${service.ping.timeoutMillis}")
+  private int timeoutMillis;
+
+  @Autowired private PingTestEventRepository repository;
+
+  public PingTestHandler() {
+    LOGGER.info("init: timeoutMillis={}", timeoutMillis);
+    inetAddressMap = new HashMap<>();
+  }
+
+  public void run(String host) {
+    try {
+      // execute ping test
+      final PingTestEvent pingTestEvent = ping(host);
+      LOGGER.info("run: {}", pingTestEvent);
+
+      // persist
+      repository.save(pingTestEvent);
+    } catch (Exception e) {
+      final String msg = String.format("Unable to run ping test on host=%s", host);
+      LOGGER.error(msg, e);
+    }
+  }
+
+  PingTestEvent ping(String host) throws IOException {
+    final InetAddress inetAddress = getInetAddress(host);
+    final long startNanos = System.nanoTime();
+    inetAddress.isReachable(timeoutMillis);
+    final long stopNanos = System.nanoTime();
+    final double elapsedMillis = (stopNanos - startNanos) / 1000000.0;
+    return getPingTestEvent(host, elapsedMillis);
+  }
+
+  InetAddress getInetAddress(String host) throws UnknownHostException {
+    if (!inetAddressMap.containsKey(host)) {
+      LOGGER.info("Initializing InetAddress {}", host);
+      inetAddressMap.put(host, InetAddress.getByName(host));
+    }
+    return inetAddressMap.get(host);
+  }
+
+  static PingTestEvent getPingTestEvent(String host, double millis) {
+    return new PingTestEvent(host, millis, System.currentTimeMillis());
+  }
+}
