5d66b47300e4b95b2ff60b362dbe0d06bbb94bef
Robert Rasgorshek <robert.rasgorshek@ngc.com>
2019-03-31 12:29:10 -0500

    sonar fixes

--- common/src/main/java/site/razz/common/util/TextUtil.java
+++ common/src/main/java/site/razz/common/util/TextUtil.java
@@ -8,11 +8,16 @@ import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class TextUtil {
+
+  private TextUtil() {
+    // empty constructor
+  }
 
   public static String encode(String text) throws UnsupportedEncodingException {
     return URLEncoder.encode(text, "UTF-8");
@@ -23,34 +28,34 @@ public class TextUtil {
   }
 
   public static String toString(InputStream inputStream, String delimiter) {
-    final String result =
-        new BufferedReader(new InputStreamReader(inputStream))
-            .lines()
-            .collect(Collectors.joining(delimiter));
-    return result;
+    return new BufferedReader(new InputStreamReader(inputStream))
+        .lines()
+        .collect(Collectors.joining(delimiter));
   }
 
-  public static String find(String regex, int group, String source, String _default) {
+  public static String find(String regex, int group, String source, String defaultValue) {
     try {
-      return find(Pattern.compile(regex), group, source, _default);
-    } catch (Exception e) {
+      return find(Pattern.compile(regex), group, source, defaultValue);
+    } catch (Exception ignore) {
+      // ignore - do nothing
     }
-    return _default;
+    return defaultValue;
   }
 
-  public static String find(Pattern pattern, int group, String source, String _default) {
+  public static String find(Pattern pattern, int group, String source, String defaultValue) {
     try {
       final Matcher matcher = pattern.matcher(source);
       if (matcher.find()) {
         return matcher.group(group);
       }
-    } catch (Exception e) {
+    } catch (Exception ignore) {
+      // ignore - do nothing
     }
-    return _default;
+    return defaultValue;
   }
 
   public static long dateToLong(String dtg, String dtgFormat) throws ParseException {
-    final SimpleDateFormat parser = new SimpleDateFormat(dtgFormat);
+    final SimpleDateFormat parser = new SimpleDateFormat(dtgFormat, Locale.US);
     final Date date = parser.parse(dtg);
     return date.getTime();
   }
